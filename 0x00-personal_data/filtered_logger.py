bbdeb8dee79f651ba80b633e7822b4b6784615cd
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-08 09:16:18 +0300

    Regex-ing fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,10 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''returns the log message obfuscated'''
-    for field in fields:
-        message = re.sub(rf'    {field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
     return message

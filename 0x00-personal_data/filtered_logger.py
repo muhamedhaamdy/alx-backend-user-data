09e8494e374514c43f577e9c2385fe8bd32119b4
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-08 09:15:29 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """Personal data module"""
 import re
+from typing import List
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
     '''returns the log message obfuscated'''
     for field in fields:
-        message = re.sub(rf'{field}=.*?{separator}',
+        message = re.sub(rf'    {field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message

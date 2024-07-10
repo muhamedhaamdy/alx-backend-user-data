bcca1e95fea8bfa0442fe84a49cc0ee84e883961
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-10 05:30:25 +0300

    Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,13 +8,33 @@ from models.user import User
 class Auth:
     '''auth class'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''require_auth method'''
-        if path and not path.endswith('/'):
-            path += '/'
-        if path is None or excluded_paths is None or len(excluded_paths) == 0\
-                or path not in excluded_paths:
+        """ Method for validating if endpoint requires auth """
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        return False
+
+        l_path = len(path)
+        if l_path == 0:
+            return True
+
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''authorization_header method'''

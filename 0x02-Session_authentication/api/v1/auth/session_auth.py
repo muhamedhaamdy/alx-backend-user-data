58d9b0f62a55ba73103d43223fd75dbbfa5ba5ea
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-12 00:05:07 +0300

    Create a session style

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,7 +4,6 @@ from api.v1.auth.auth import Auth
 import uuid
 
 
-
 class SessionAuth(Auth):
     ''' session_auth module '''
     user_id_by_session_id = {}
@@ -12,11 +11,8 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         '''creating session'''
 
-        if user_id == None or not isinstance(user_id, str):
-           return None
+        if user_id is None or not isinstance(user_id, str):
+            return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-        
-
-

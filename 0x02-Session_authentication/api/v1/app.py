7ac8138aa47115c13f69f0907969ce2daa0eb1d1
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-12 00:31:38 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -70,7 +70,7 @@ def before_request() -> None:
     req = auth.require_auth(request.path, paths)
     if req:
         if not auth.authorization_header(request)\
-            and not auth.session_cookie(request)    :
+               and not auth.session_cookie(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)

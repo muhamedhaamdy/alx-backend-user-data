8cd55b2812e0c13960cc5651491f9567cb60b30a
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-17 01:03:13 +0300

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -16,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -37,5 +39,22 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
-
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user by arbitrary keyword arguments.
+        Args:
+            **kwargs: Arbitrary keyword arguments.
+        Returns:
+            User: The first user found matching the arguments.
+        Raises:
+            NoResultFound: If no user is found.
+            InvalidRequestError: If invalid arguments are provided.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the provided filter.")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid filter arguments provided.")
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -9,7 +9,7 @@ from user import User
 my_db = DB()
 
 user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.email, user_1.id, user_1.hashed_password)
+print(user_1.id)
 
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2)
+print(user_2.id)

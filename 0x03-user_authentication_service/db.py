80e382295a1b9f67bc686c1ce9cab72e3d3b74a7
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-17 00:22:48 +0300

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -30,10 +29,10 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         ''' appending user to database '''
-        user = User(email = email, hashed_password = hashed_password)
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user

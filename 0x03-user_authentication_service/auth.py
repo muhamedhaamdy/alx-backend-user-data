9d3459d0898b07042d0eda208be58a30abdb53b7
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-19 01:32:21 +0300

    Register user style

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,17 +11,19 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-    
+
     def register_user(self, email: str, password: str) -> User:
         ''' signup for the user with email and password '''
-        find_user = self._db._session.query(User).filter_by(email=email).first()
+        find_user =\
+            self._db._session.query(User).filter_by(email=email).first()
         if find_user:
             raise ValueError(f'User {find_user.email} already exists')
         else:
             hash = _hash_password(password)
             new_user = User(email=email, hashed_password=hash)
             self._db._session.add(new_user)
-            self._db._session.commit() 
+            self._db._session.commit()
+
 
 def _hash_password(password: str) -> bytes:
     ''' return a hashed password '''

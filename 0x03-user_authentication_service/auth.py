ccdc103867be68aac44b9f0c9c194142b95904ae
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-19 04:03:28 +0300

    Update password fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,19 +74,17 @@ class Auth:
         return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Uses reset token to validate update of users password"""
+        """Update the user password given the reset token
+        """
         if reset_token is None or password is None:
             return None
-
         try:
             user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password,
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
-
-        hashed_password = _hash_password(password)
-        self._db.update_user(user.id,
-                             hashed_password=hashed_password,
-                             reset_token=None)
 
 
 def _hash_password(password: str) -> bytes:

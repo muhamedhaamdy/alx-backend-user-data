ba224caafb7844028762fa495b17543d02a178ca
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-19 03:17:44 +0300

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,6 +56,10 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        ''' destroy the session '''
+        self._db.update_user(user_id, session_id=None)
+
 
 def _hash_password(password: str) -> bytes:
     ''' return a hashed password '''

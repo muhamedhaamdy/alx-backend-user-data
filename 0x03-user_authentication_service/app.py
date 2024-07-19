410eb29f73aee63ba993558c97ed699d8ef607e2
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-19 03:12:52 +0300

    Find user by session ID

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -39,21 +39,6 @@ def login():
     else:
         abort(401)
 
-# @app.route('/sessions', methods=['POST'], strict_slashes=False)
-# def login():
-#     email = request.form.get('email')
-#     password = request.form.get('password')
-#     try:
-#         if AUTH.valid_login(email, password):
-#             session_id = AUTH.create_session(email)
-#             response = jsonify({"email": email, "message": "logged in"})
-#             response.set_cookie('session_id', session_id)
-#             return response
-#         else:
-#             abort(401)
-#     except NoResultFound:
-#         return jsonify({"message": "user not found"}), 404
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,6 +47,10 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        ''' get user by session id '''
+        return self._db.find_user_by(session_id=session_id)
+
 
 def _hash_password(password: str) -> bytes:
     ''' return a hashed password '''

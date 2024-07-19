88db21ad5b6d1bae3e7c99f7e8215b333258ac51
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-19 03:08:11 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''flask app'''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 
 app = Flask(__name__)
@@ -24,6 +25,34 @@ def register():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
+
+# @app.route('/sessions', methods=['POST'], strict_slashes=False)
+# def login():
+#     email = request.form.get('email')
+#     password = request.form.get('password')
+#     try:
+#         if AUTH.valid_login(email, password):
+#             session_id = AUTH.create_session(email)
+#             response = jsonify({"email": email, "message": "logged in"})
+#             response.set_cookie('session_id', session_id)
+#             return response
+#         else:
+#             abort(401)
+#     except NoResultFound:
+#         return jsonify({"message": "user not found"}), 404
 
 
 if __name__ == "__main__":

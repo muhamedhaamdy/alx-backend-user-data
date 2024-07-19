dfe4468a0a310360fa98d044691a51b22333f29b
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-19 03:15:23 +0300

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,7 +49,12 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         ''' get user by session id '''
-        return self._db.find_user_by(session_id=session_id)
+        if not session_id:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
 
 
 def _hash_password(password: str) -> bytes:
